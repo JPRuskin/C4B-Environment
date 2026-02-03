6e925384f9d3ed6c7b0fcaae2ba5bedd850b8bfe
James Ruskin <james@chocolatey.io>
2026-02-03 12:19:17 +0000

    Adds Basic Build

--- Pack.ps1
+++ Pack.ps1
@@ -9,8 +9,6 @@ param(
     )
 )
 
-# Compress-Archive -Path "$PSScriptRoot\$SemVer\" -DestinationPath "$PSScriptRoot\C4B-Environment.$SemVer.zip"
-
 choco pack $PSScriptRoot\chocolatey\c4b-environment.nuspec --Version $SemVer --OutputDirectory $PSScriptRoot --limit-output | Write-Verbose
 
 Get-Item "$PSScriptRoot\c4b-environment.powershell.$SemVer.nupkg"
